2289c00ff26e2103759ae535a19888ce6658b195
Anatoliy Dubovik <huykilik@gmail.com>
2022-04-02 08:56:41 +0300

    Fix Homework 02-01

--- module02/02-01/002/main.go
+++ module02/02-01/002/main.go
@@ -14,16 +14,14 @@ var (
 	p  = 1.609
 	v1 = 120.4
 	v2 = 130
-	vA AmericanVelocy
-	vE EuropeanVelocy
 )
 
 func main() {
 	mToKm := p / 3600
-	vA = AmericanVelocy(v2) * AmericanVelocy(mToKm)
+	vA := AmericanVelocy(v2) * AmericanVelocy(mToKm)
 	fmt.Println(vA)
 	fmt.Println("Округляем до 2 знаков: ", math.Round((float64(vA))*100)/100)
-	vE = EuropeanVelocy(v1) * EuropeanVelocy(mToKm)
+	vE := EuropeanVelocy(v1) * EuropeanVelocy(mToKm)
 	fmt.Println(vE)
 	fmt.Println("Округляем до 2 знаков: ", math.Round((float64(vE))*100)/100)
 }
