a52ce71d0318f503d7d36733d18deeb02a1e3488
Anatoliy Dubovik <huykilik@gmail.com>
2022-05-31 09:31:29 +0300

    Solution Homework 06_01_v2

--- module06/internal/pkg/util/util_test.go
+++ module06/internal/pkg/util/util_test.go
@@ -1,26 +1,22 @@
 package util
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestReversInt(t *testing.T) {
+	req := require.New(t)
 	res0, err := ReverseInt(123)
-	if err != nil {
-		t.Error("Fail to get input: %w\n", err)
-	}
-	if res0 != 321 {
-		t.Error("incorrect result\n")
-	}
-	res1, _ := ReverseInt(-649)
-	fmt.Println(res1)
-	if res1 != -946 {
-		t.Fatal("incorrect negativ result\n")
-	}
-	res2, _ := ReverseInt(0)
-	if res2 != 0 {
-		t.Error("Revers 0 != 0\n")
-	}
+	req.NoError(err)
+	req.Equal(321, res0)
 
+	res1, err := ReverseInt(-649)
+	req.NoError(err)
+	req.Equal(-946, res1)
+
+	res2, err := ReverseInt(0)
+	req.NoError(err)
+	req.Equal(0, res2)
 }
