fa75e7a846c547164669ca679d1acc1656b762a8
Anatoliy Dubovik <huykilik@gmail.com>
2022-06-02 14:34:57 +0300

    Solution Homework06_02

---
+++ module06/test/gomock/mocks/gomock.go
@@ -0,0 +1,3 @@
+package gomock
+
+//go:generate mockgen -source=C:/Users/ToJIuK/go/src/Rebrain/Basic/reb_Basic/module06/internal/app/services/post/client.go -destination=C:/Users/ToJIuK/go/src/Rebrain/Basic/reb_Basic/module06/test/gomock/mocks/postmock/post_counters_gomock.go -package=postmock
